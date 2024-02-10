fb0dbe957a0635af2e7a12283da6248ccb43a453
alima <alimaseid72@gmail.com>
2024-02-10 09:01:08 +0300

    update sidebar

--- resources/views/Frames/app.blade.php
+++ resources/views/Frames/app.blade.php
@@ -43,7 +43,7 @@
     <link rel="stylesheet" href="../../../vendor/bs-select/bs-select.css" />
     <!-- jQcloud Keywords css -->
     <link rel="stylesheet" href="../../../vendor/jqcloud/jqcloud.css" />
-    
+
     <style>
         /* For Firefox */
         /* For Firefox */
@@ -55,6 +55,13 @@
         /* For Chrome, Safari, and Opera */
         .sidebar-menu::-webkit-scrollbar {
             width: 6px;
+        }
+
+        /* Adjust scrollbar width for smaller screens */
+        @media screen and (max-width: 768px) {
+            .sidebar-menu::-webkit-scrollbar {
+                width: 3px;
+            }
         }
 
         .sidebar-menu::-webkit-scrollbar-thumb {
@@ -71,7 +78,7 @@
             /* Enable smooth scrolling on iOS devices */
         }
     </style>
-     @livewireStyles
+    @livewireStyles
 </head>
 
 <body>
@@ -80,54 +87,22 @@
     <div class="page-wrapper">
         @php
 
-            $roleList = auth()
-                ->user()
-                ->can('role-list');
-            $userList = auth()
-                ->user()
-                ->can('user-list');
-            $waitstaffList = auth()
-                ->user()
-                ->can('waitstaff-list');
-            $tableList = auth()
-                ->user()
-                ->can('table-list');
-            $supplierList = auth()
-                ->user()
-                ->can('supplier-list');
-            $reservationList = auth()
-                ->user()
-                ->can('reservation-list');
-            $supplyItemList = auth()
-                ->user()
-                ->can('supplyItem-list');
-            $reportList = auth()
-                ->user()
-                ->can('report-manage');
-            $purchaseList = auth()
-                ->user()
-                ->can('purchase-list');
-            $orderList = auth()
-                ->user()
-                ->can('order-list');
-            $menuItemList = auth()
-                ->user()
-                ->can('menuItem-list');
-            $locationList = auth()
-                ->user()
-                ->can('location-list');
-            $supplyItemCategoryList = auth()
-                ->user()
-                ->can('supplyItemCategory-list');
-            $issuingList = auth()
-                ->user()
-                ->can('issuing-list');
-            $menuCategoryList = auth()
-                ->user()
-                ->can('menuCategory-list');
-                $kitchenList = auth()
-                ->user()
-                ->can('kitchen-list');
+            $roleList = auth()->user()->can('role-list');
+            $userList = auth()->user()->can('user-list');
+            $waitstaffList = auth()->user()->can('waitstaff-list');
+            $tableList = auth()->user()->can('table-list');
+            $supplierList = auth()->user()->can('supplier-list');
+            $reservationList = auth()->user()->can('reservation-list');
+            $supplyItemList = auth()->user()->can('supplyItem-list');
+            $reportList = auth()->user()->can('report-manage');
+            $purchaseList = auth()->user()->can('purchase-list');
+            $orderList = auth()->user()->can('order-list');
+            $menuItemList = auth()->user()->can('menuItem-list');
+            $locationList = auth()->user()->can('location-list');
+            $supplyItemCategoryList = auth()->user()->can('supplyItemCategory-list');
+            $issuingList = auth()->user()->can('issuing-list');
+            $menuCategoryList = auth()->user()->can('menuCategory-list');
+            $kitchenList = auth()->user()->can('kitchen-list');
         @endphp
 
         <!-- Sidebar wrapper start -->
@@ -439,7 +414,7 @@
 
 
     @livewireScripts
-	
+
     <!-- Page wrapper end -->
 
     <!--**************************
@@ -543,12 +518,12 @@
             document.getElementById('success-alert').style.display = 'none';
         }, 5000);
     </script>
-     <script>
+    <script>
         setTimeout(function() {
             document.getElementById('danger-alert').style.display = 'none';
         }, 5000);
     </script>
-     <script>
+    <script>
         setTimeout(function() {
             document.getElementById('warning-alert').style.display = 'none';
         }, 5000);
